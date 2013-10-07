364ff793080fe8b000517d1d524462617bbc8b8d
Arnar Yngvason <arnar@hvitahusid.is>
2013-10-07 11:22:52 +0000

    sprites

--- js/main.js
+++ js/main.js
@@ -1,33 +1,48 @@
 $(document).ready(function() {
 
-	$('#photos').find('li .text').each(function() {
-		var words = $(this).html().split(' ');
-		for(var i in words) {
-			if(words[i][0] === '#' || words[i][0] === '@') {
-				words[i] = '<span class="hashtag">' + words[i] + '</a>';
-			}
-		}
-		$(this).html(words.join(' '));
-	});
+    $('#photos').find('li .text').each(function() {
+        var words = $(this).html().split(' ');
+        for(var i in words) {
+            if(words[i][0] === '#' || words[i][0] === '@') {
+                words[i] = '<span class="hashtag">' + words[i] + '</span>';
+            }
+        }
+        $(this).html(words.join(' '));
+    });
 
-	var interval = setInterval(scroll, 4000);
+    var interval = setInterval(scroll, 7000);
 
+    sprites();
+    var sprites_interval = setInterval(sprites, 7000);
+
+    $(document).click(function(){
+        window.open('http://www.forvarnardagur.is/', '_blank').focus();
+    });
 });
 
 function scroll() {
-	var offset = 24,
-		$active = $('#photos li.active'),
-		$next = $active.next();
+    var offset = 24,
+        $active = $('#photos li.active'),
+        $next = $active.next();
 
-	if(!$next.length) {
-		$next = $('#photos').find('li').first();
-	}
+    if(!$next.length) {
+        $next = $('#photos').find('li').first();
+    }
 
-	$active.removeClass('active');
-	$next.addClass('active');
+    $active.removeClass('active');
+    $next.addClass('active');
 
-	var current_pos = parseInt($('#photos').css('margin-top'), 10);
-	var next_pos = $next.offset().top;
+    var current_pos = parseInt($('#photos').css('margin-top'), 10);
+    var next_pos = $next.offset().top;
 
-	$('#photos').animate({'margin-top': -(-current_pos + next_pos + offset)}, 500, 'easeOutQuad');
+    $('#photos').animate({'margin-top': -(-current_pos + next_pos + offset)}, 500, 'easeOutQuad');
+}
+
+function sprites() {
+    $('#photos li .image')
+        .animate({backgroundPosition:0}, 0).delay(2000)
+        .animate({backgroundPosition:-265}, 0).delay(100)
+        .animate({backgroundPosition:-530}, 0).delay(2000)
+        .animate({backgroundPosition:-265}, 0).delay(100)
+        .animate({backgroundPosition:0}, 0);
 }
